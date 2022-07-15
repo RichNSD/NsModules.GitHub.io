af3a170bfaedb54710d8ee518efe58b96ac25e20
Richard Joubert <richard@nsd.academy>
2022-07-15 19:16:17 -0400

    cheese

--- NsOpenSSH/module/contents/OpenSSH/scripts/NsOpenSSH_Install.ps1
+++ NsOpenSSH/module/contents/OpenSSH/scripts/NsOpenSSH_Install.ps1
@@ -26,14 +26,14 @@ function Get-NsOpenSSH {
 
 
 		# VARIABLES
-			New-Variable -Name 'OpenSSHgit' -Value "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.9.0.0p1-Beta/OpenSSH-Win64.zip" -Scope 'Local' -ErrorAction 'Suspend' -Description "The download URL for the OpenSSH client, hosted by GitHub." -WarningVariable +NsWarningVar -ErrorVariable +NsErrorVar -InformationVariable +NsInfoVar -PassThru
+			New-Variable -Name 'OpenSSHgit' -Value "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.9.0.0p1-Beta/OpenSSH-Win64.zip" -Scope 'Local' -ErrorAction 'Stop' -Description "The download URL for the OpenSSH client, hosted by GitHub." -WarningVariable +NsWarningVar -ErrorVariable +NsErrorVar -InformationVariable +NsInfoVar -PassThru
 
-			New-Variable -Name 'scriptStage' -Value "C:\$HOME\.stage" -Scope 'Local' -ErrorAction 'Suspend' -Description "This hidden, temporary folder is (as the name suggests) is meant to provide a space in which files and directories can be organized, modified, etc. As the script completes, the folder will be removed- so if you (if 'you' are the user) find it, just delete it." -WarningVariable +NsWarningVar -ErrorVariable +NsErrorVar -InformationVariable +NsInfoVar -PassThru
+			New-Variable -Name 'scriptStage' -Value "$HOME\.stage" -Scope 'Local' -ErrorAction 'Stop' -Description "This hidden, temporary folder is (as the name suggests) is meant to provide a space in which files and directories can be organized, modified, etc. As the script completes, the folder will be removed- so if you (if 'you' are the user) find it, just delete it." -WarningVariable +NsWarningVar -ErrorVariable +NsErrorVar -InformationVariable +NsInfoVar -PassThru
 				# After this directory is created, a 'README.txt' document with the same content as the above description needs to be created with it in the event this (or any of our other scripts) winds up failing before the directory is removed.
 			
-			New-Variable -Name 'OpenSshDir' -Value "$ProgramFiles\OpenSSH" -Scope 'Local' -ErrorAction 'Suspend' -Description "The primary working driectory for the OpenSSH client, located in 'Program Files'." -WarningVariable +NsWarningVar -ErrorVariable +NsErrorVar -InformationVariable +NsInfoVar -PassThru
+			New-Variable -Name 'OpenSshDir' -Value "C:\Program Files\OpenSSH" -Scope 'Local' -ErrorAction 'Stop' -Description "The primary working driectory for the OpenSSH client, located in 'Program Files'." -WarningVariable +NsWarningVar -ErrorVariable +NsErrorVar -InformationVariable +NsInfoVar -PassThru
 
-			New-Variable -Name 'HostSshData' -Value "$HOME\.ssh" -Scope 'Local' -ErrorAction 'Suspend' -Description "Variable Description." -WarningVariable +NsWarningVar -ErrorVariable +NsErrorVar -InformationVariable +NsInfoVar -PassThru
+			New-Variable -Name 'HostSshData' -Value "$HOME\.ssh" -Scope 'Local' -ErrorAction 'Stop' -Description "Variable Description." -WarningVariable +NsWarningVar -ErrorVariable +NsErrorVar -InformationVariable +NsInfoVar -PassThru
 
 
 
@@ -45,8 +45,9 @@ function Get-NsOpenSSH {
 		# This needs to be SIGNIFICANTLY more complicated (conditions, , error checking, etc)
 		Invoke-WebRequest -Uri "$OpenSSHgit" -OutFile "$HOME\Downloads\OpenSSH.zip" -Verbose -PassThru
 
-		New-Item -ItemType Directory -Path "$scriptStage" -Force
-		Expand-Archive -LiteralPath "$Home\Downloads\OpenSSH.zip" -DestinationPath ""
+		New-Item -ItemType Directory -Path "$scriptStage" -Force -WarningVariable +NsWarningVar -ErrorVariable +NsErrorVar -InformationVariable +NsInfoVar
+		Expand-Archive -LiteralPath "$Home\Downloads\OpenSSH.zip" -DestinationPath "$sciptStage\OpenSSH-Stage" -WarningVariable +NsWarningVar -ErrorVariable +NsErrorVar -InformationVariable +NsInfoVar -PassThru
+
 
 	}
 
