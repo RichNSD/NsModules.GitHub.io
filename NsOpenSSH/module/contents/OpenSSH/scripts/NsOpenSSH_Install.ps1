989a9994775870a9a9aa1f761aa6dec92bb4545b
Richard Joubert <richard@nsd.academy>
2022-07-15 22:26:09 -0400

    update NsOpenSSH_Install.ps1

--- NsOpenSSH/module/contents/OpenSSH/scripts/NsOpenSSH_Install.ps1
+++ NsOpenSSH/module/contents/OpenSSH/scripts/NsOpenSSH_Install.ps1
@@ -17,23 +17,27 @@ function Get-NsOpenSSH {
 		[string[]]$DownloadDir
 	)
 	
-	func
-
+	
 	begin {
 
 		$WarningPreference="SilentlyContinue"
 		$ErrorView="CategoryView"
 
+		
+		
+		
 
 		# VARIABLES
 			New-Variable -Name 'OpenSSHgit' -Value "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.9.0.0p1-Beta/OpenSSH-Win64.zip" -Scope 'Local' -ErrorAction 'Stop' -Description "The download URL for the OpenSSH client, hosted by GitHub." -WarningVariable +NsWarningVar -ErrorVariable +NsErrorVar -InformationVariable +NsInfoVar -PassThru
 
-			New-Variable -Name 'scriptStage' -Value "$HOME\.stage" -Scope 'Local' -ErrorAction 'Stop' -Description "This hidden, temporary folder is (as the name suggests) is meant to provide a space in which files and directories can be organized, modified, etc. As the script completes, the folder will be removed- so if you (if 'you' are the user) find it, just delete it." -WarningVariable +NsWarningVar -ErrorVariable +NsErrorVar -InformationVariable +NsInfoVar -PassThru
+			New-Variable -Name 'scriptStage' -Value "$C:\.stage" -Scope 'Local' -ErrorAction 'Stop' -Description "This hidden, temporary folder is (as the name suggests) is meant to provide a space in which files and directories can be organized, modified, etc. As the script completes, the folder will be removed- so if you (if 'you' are the user) find it, just delete it." -WarningVariable +NsWarningVar -ErrorVariable +NsErrorVar -InformationVariable +NsInfoVar -PassThru
 				# After this directory is created, a 'README.txt' document with the same content as the above description needs to be created with it in the event this (or any of our other scripts) winds up failing before the directory is removed.
 			
 			New-Variable -Name 'OpenSshDir' -Value "C:\Program Files\OpenSSH" -Scope 'Local' -ErrorAction 'Stop' -Description "The primary working driectory for the OpenSSH client, located in 'Program Files'." -WarningVariable +NsWarningVar -ErrorVariable +NsErrorVar -InformationVariable +NsInfoVar -PassThru
 
 			New-Variable -Name 'HostSshData' -Value "$HOME\.ssh" -Scope 'Local' -ErrorAction 'Stop' -Description "Variable Description." -WarningVariable +NsWarningVar -ErrorVariable +NsErrorVar -InformationVariable +NsInfoVar -PassThru
+			
+			
 
 
 
@@ -42,7 +46,6 @@ function Get-NsOpenSSH {
 
 	process { # For some reason VS Code thinks I'm breakibng the sacred rules of the 'process' block
 
-		# This needs to be SIGNIFICANTLY more complicated (conditions, , error checking, etc)
 		Invoke-WebRequest -Uri "$OpenSSHgit" -OutFile "$HOME\Downloads\OpenSSH.zip" -Verbose -PassThru
 
 		New-Item -ItemType Directory -Path "$scriptStage" -Force -WarningVariable +NsWarningVar -ErrorVariable +NsErrorVar -InformationVariable +NsInfoVar
@@ -87,13 +90,28 @@ function Get-NsOpenSSH {
 
 #>
 
-{0}
-
-}
 
 
 
 
 
-# [CmdletBinding(SupportShouldProcess)]
-# https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.9.0.0p1-Beta/OpenSSH-Win64.zip
+
+
+
+
+<#
+
+===|  Temporary Code Space  |===
+
+[CmdletBinding(SupportShouldProcess)]
+
+
+Invoke-WebRequest -Uri "$OpenSSHgit" -OutFile "$HOME\Downloads\OpenSSH.zip" -Verbose -PassThru
+
+The ⬆️ 'Invoke-WebRequest' ⬆️ above needs to be SIGNIFICANTLY more complicated (conditions, error checking, etc).
+
+
+https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.9.0.0p1-Beta/OpenSSH-Win64.zip
+
+
+#>
